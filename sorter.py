a35393325a20adae82930e6cea8bbfa41bfd7737
Anton Golovenko <golan349@gmail.com>
2021-04-08 20:03:49 +0500

    Solve trouble with Russina symbols in PATH and made big refactor

--- sorter.py
+++ sorter.py
@@ -1,80 +1,68 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
-import os
 import click
+import os
 import eyed3
 
+import shutil
 
 @click.command()
+
 @click.option('-s', '--src-dir',
-              type=click.Path(),
+              default='.',
               help='Source directory.',
-              default=os.getcwd())
+              show_default=True)
 @click.option('-d', '--dst-dir',
-              type=click.Path(),
+              default='.',
               help='Destination directory.',
-              default=os.getcwd())
-def main(src_dir: str, dst_dir: str) -> None:
-    """[Author - Golovenko Anton] MP3 files sorter program. 2021 (c)"""
+              show_default=True)
 
-    eyed3.log.setLevel("ERROR")
-    if os.path.exists(src_dir):
-        files = os.listdir(src_dir)
-        files_list = list(filter(lambda x: x.endswith('.mp3'), files))
-        if files_list:
-            for file_name in files_list:
-                try:
-                    audio_file = eyed3.load(file_name)
-                    title = audio_file.tag.title.strip() if audio_file.tag.title else None
-                    singer = audio_file.tag.artist.strip() if audio_file.tag.artist else None
-                    album = audio_file.tag.album.strip() if audio_file.tag.album else None
-
-                except AttributeError as e:
-                    print(f'Something wrong with file: {file_name}')
-                except PermissionError as e:
-                    print(f'Have no right to change file: {file_name}')
-                    continue
-                new_filename = generate_new_name(title, singer, album)
-                if new_filename is None:
-                    new_filename = file_name
-
-                if singer and album:
-                    new_path = os.path.join(singer, album)
-                else:
-                    continue
-
-                new_file = os.path.join(dst_dir, new_path, new_filename)
-                try:
-                    os.renames(file_name, new_file)
-                    print(f'{file_name} -> {new_file}')
-
-                except Exception as e:
-                    print(e)
+def sort(src_dir, dst_dir):
+    if os.path.isdir(src_dir):
+        try:
+            dir_list = os.scandir(src_dir)
+        except PermissionError as e:
+            print(str(e))
         else:
-            print('ERROR: There are no *.mp3 files on the specified path')
+            with dir_list:
+                for file_name in dir_list:
+                    if file_name.name.lower().endswith('.mp3'):
+                        try:
+                            track = eyed3.load(file_name)
+                            if not track.tag.title:
+                                title = file_name.name
+                            else:
+                                title = track.tag.title.replace('/', '_')
+                            if not track.tag.artist or not track.tag.album:
+                                print(f'Not enough tags: {file_name.name}')
+                                continue
+                            else:
+                                artist = track.tag.artist.replace('/', '_')
+                                album = track.tag.album.replace('/', '_')
+                            #track.tag.save()
+                        except AttributeError as e:
+                            print(f'Something wrong with file: {file_name.name}')
+                        except PermissionError as e:
+                            print(f'Have no permits to change file: {file_name.name}')
+                            continue
+                        else:
+                            new_file_name = f'{title} - {artist} - {album}.mp3'
+                            if os.path.exists(os.path.join(dst_dir, artist, album)):
+                                shutil.move(os.path.join(src_dir, file_name.name),
+                                            os.path.join(dst_dir, artist, album, new_file_name))
+                            else:
+                                try:
+                                    os.makedirs(os.path.join(dst_dir, artist, album))
+                                except PermissionError as e:
+                                    print(str(e))
+                                else:
+                                    shutil.move(os.path.join(src_dir, file_name.name),
+                                                os.path.join(dst_dir, artist, album, new_file_name))
+                            print(f'{os.path.join(src_dir, file_name.name)} '
+                                  f'-> {os.path.join(dst_dir, artist, album, new_file_name)}')
+        print ("[INFO] Done.")
     else:
-        print('ERROR: Source directory not exist')
-    print('[INFO] Done.')
+        print('Source directory not found.')
 
 
-def file_has_permissions(filename):
-    try:
-        with open(filename, 'rb') as f:
-            f.readline()
-        return True
-
-    except PermissionError:
-        print(f'Has not file access permissions {filename}')
-        return False
-
-
-def generate_new_name(title, singer, album):
-    if title:
-        return f'{title} - {singer} - {album}.mp3'
-    else:
-        return None
-
-
-if __name__ == '__main__':
-    main()
+sort()
